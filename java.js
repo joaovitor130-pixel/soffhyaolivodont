2d3d6d60a08378470e8e9cb53fba3db3bd500873
joaovitor130-pixel <joao.vitor.130@ufrn.edu.br>
2026-03-22 11:59:17 -0300

    Add files via upload

---
+++ java.js
@@ -0,0 +1,5 @@
+function toggleMenu() {
+            // Seleciona o menu e alterna a classe 'active'
+            const nav = document.querySelector('nav');
+            nav.classList.toggle('active');
+        }
